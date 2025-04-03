1708407bde9b151b9f7869e067c461f92e7939d3
Ruselmp3 <juses0981@gmail.com>
2025-04-03 13:47:38 +0300

    dz_3_3

--- lesson3/lesson3.3.py
+++ lesson3/lesson3.3.py
@@ -1,20 +1,21 @@
-lst = [1,2,3,4,5,6]
+lst = [1, 2, 3, 4, [3,4,5], "Hello"]
 
 x = len(lst)
 my_list = x // 2
 
-if x == 0:
-    print("Лист порожній")
-elif x % 2 == 0:
+if not x:
+    result = [lst, lst]
+    print(result)
+elif not x % 2:
     half_list1 = lst[:my_list]
     half_list2 = lst[my_list:]
-    print(half_list1)
-    print(half_list2)
-elif x % 2 != 0:
-    half_list1 = lst[:my_list+1]
-    half_list2 = lst[my_list+1:]
-    print(half_list1)
-    print(half_list2)
+    result = [half_list1, half_list2]
+    print(result)
+else:
+    half_list1 = lst[:my_list + 1]
+    half_list2 = lst[my_list + 1:]
+    result = [half_list1, half_list2]
+    print(result)
 
 
 
