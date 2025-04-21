c58ca3ce6deb390aede6a8d9604a44b33fc70bba
Ruselmp3 <juses0981@gmail.com>
2025-04-21 22:59:52 +0300

    dz_5.1

--- lesson5/lesson5.1.py
+++ lesson5/lesson5.1.py
@@ -12,16 +12,14 @@ elif any(char.isupper() for char in name):
 elif name in keyword.kwlist:
     print("False")
 
+elif any(char in string.punctuation and char != "_" for char in name):
+    print("False")
+
+elif name.startswith('__') or name.startswith('___'):
+    print("False")
+
+elif ' ' in name:
+    print("False")
+
 else:
-    wrong_chars = string.punctuation.replace("_", "") + " "
-    if any(char in wrong_chars for char in name):
-        print("False")
-    else:
-        underscore_count = 0
-        for char in name:
-            if char == "_":
-                underscore_count += 1
-        if underscore_count > 1:
-            print("False")
-        else:
-            print("True")
+    print("True")
