5393a2a1143f40e20aa560920ae79d880e76f73a
Ruselmp3 <juses0981@gmail.com>
2025-04-23 16:50:50 +0300

    dz_7.3

---
+++ lesson7/lesson7.3.py
@@ -0,0 +1,12 @@
+def second_index(text: str, some_str: str):
+    _first = text.index(some_str)
+    _second = text.find(some_str, _first+1)
+    if _second != -1:
+        return _second
+    else:
+        return None
+
+assert second_index("sims","s") == 3, 'Test 1'
+assert second_index("find the river", "e") == 12, 'Test 2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test 4'
