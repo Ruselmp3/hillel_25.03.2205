d12df2472f24341559280e8e50d75c74a2c579c1
Ruselmp3 <juses0981@gmail.com>
2025-04-23 18:24:32 +0300

    dz_7.4

---
+++ lesson7/lesson7.4.py
@@ -0,0 +1,9 @@
+def common_elements():
+
+    del_3 = {i for i in range(100) if i % 3 == 0}
+    del_5 = {i for i in range(100) if i % 5 == 0}
+    common = del_3 & del_5
+
+    return common
+
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
