22ea704451ec52a663d188b883705ea8f0473b86
Ruselmp3 <juses0981@gmail.com>
2025-05-26 17:23:52 +0300

    dz_14.2

---
+++ lesson14/lesson14.2/main.py
@@ -0,0 +1,17 @@
+from Group import *
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+print(gr)
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+assert gr.find_student('Jobs') == st1
+
+gr.delete_student('Taylor')
+print(gr)  # Only one student
+
+gr.delete_student('Taylor')  # No error!
